dad59bf0e06cf032232d186d92cefe83cb5542a9
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-07-31 11:27:44 +0900

    Pagination 구현

--- kcg/views.py
+++ kcg/views.py
@@ -1,21 +1,49 @@
 from rest_framework import viewsets
 from kcg.models import Triple
 from kcg.serializers import TripleSerializer
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from django.core.paginator import Paginator
+import ujson as json
 
-class KcgViewSet(viewsets.ReadOnlyModelViewSet):
+@csrf_exempt
+def triple_list(request):
     """
-    List all KCG triples. 
-    Provides `list` and `detail` actions.
+    List all KCG triples.
     """
-    queryset = Triple.objects.all()
-    serializer_class = TripleSerializer
+    def get_triples_with_criteria(request, criteria):
+        _filter = {}
+        for field in ['sbj', 'rel', 'obj']:
+            if field in criteria:
+                _filter[f'{field}__name__iexact'] = criteria[field]
+        for field in ['sbj_id', 'rel_id', 'obj_id']:
+            if field in criteria:
+                _filter[f'{field}__id__iexact'] = criteria[field]
 
-# @csrf_exempt
-# def triple_list(request):
+        # filter
+        triples = None
+        if _filter:
+            triples = Triple.objects.filter(**_filter)
+        else:
+            triples = Triple.objects.all()
+
+        return triples
+
+    if request.method == 'GET':
+        triples = get_triples_with_criteria(request, request.GET)
+        # paginate
+        page = request.GET.get('page', 1)
+        limit = request.GET.get('limit', 10)
+        paginated = Paginator(triples, limit).get_page(page)
+        serializer = TripleSerializer(paginated, many=True)
+        return HttpResponse(json.dumps(serializer.data, ensure_ascii=False),
+                            content_type="application/json; charset=utf-8")
+
+
+# class KcgViewSet(viewsets.ReadOnlyModelViewSet):
 #     """
 #     List all KCG triples.
+#     Provides `list` and `detail` actions.
 #     """
-#     if request.method == 'GET':
-#         triples = Triple.objects.all()
-#         serializer = TripleSerializer(triples, many=True)
-#         return JsonResponse(serializer.data, safe=False)
+#     queryset = Triple.objects.all()
+#     serializer_class = TripleSerializer
