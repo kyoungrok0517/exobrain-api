e53d1062926de9b4b5d601deee3718f6a7c0c056
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-07-31 16:09:03 +0900

    page_size -> limit

--- kcg/views.py
+++ kcg/views.py
@@ -6,7 +6,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class KcgViewSetPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'page_size'
+    page_size_query_param = 'limit'
 
 
 class KcgViewSet(viewsets.ModelViewSet):
